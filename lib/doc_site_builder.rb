59aaf9651e7b662f3f47f415a090868d0ab4d451
Jason Frey <fryguy9@gmail.com>
2015-08-03 16:01:58 -0400

    Revert "Deprecate old gem and name"
    This reverts commit 43c228b9f4d53012a1d976abb76172c2c3ced572.

--- lib/doc_site_builder.rb
+++ lib/doc_site_builder.rb
@@ -1,4 +1,3 @@
-warn "[DEPRECATION] This gem has been renamed to ascii_binder and will no longer be supported. Please switch to ascii_binder as soon as possible."
 require "doc_site_builder/version"
 require "doc_site_builder/template_renderer"
 require "doc_site_builder/helpers"
