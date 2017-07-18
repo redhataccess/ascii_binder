47f7441980469d7f145002e328a15d5164e3a376
N. Harrison Ripps <nhr@redhat.com>
2017-07-17 20:15:34 -0400

    Fixes a rakefile issue resulting from the refactor

--- lib/ascii_binder/tasks/tasks.rb
+++ lib/ascii_binder/tasks/tasks.rb
@@ -1,18 +1,21 @@
 require 'rake'
 require 'ascii_binder'
 
+include AsciiBinder::Engine
 include AsciiBinder::Helpers
 
 desc "Build the documentation"
 task :build, :build_distro do |task,args|
   # Figure out which distros we are building.
   # A blank value here == all distros
+  set_docs_root_dir(git_root_dir)
   build_distro = args[:build_distro] || ''
   generate_docs(:all,build_distro,nil)
 end
 
 desc "Package the documentation"
 task :package, :package_site do |task,args|
+  set_docs_root_dir(git_root_dir)
   package_site = args[:package_site] || ''
   Rake::Task["clean"].invoke
   Rake::Task["build"].invoke
@@ -21,6 +24,7 @@ end
 
 desc "Build the documentation and refresh the page"
 task :refresh_page, :single_page do |task,args|
+  set_docs_root_dir(git_root_dir)
   generate_docs(:working_only,'',args[:single_page])
 end
 
